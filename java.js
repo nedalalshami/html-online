5a653e44d5b132a804623efe38806325e414bf70
om omar <124066052+nedalalshami@users.noreply.github.com>
2024-02-03 22:25:18 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+let cods = document.getElementById("cods");
+let play = document.getElementById("play");
+let remove = document.getElementById("remove");
+let result = document.getElementById("result");
+
+play.onclick =()=>{
+    result.innerHTML= cods.value;
+};
+
+remove.onclick =()=>{
+    result.innerHTML= "";
+};
+
+
+
+var AppObject = new App
+
+
+let buttons =document.getElementById("buttons");
+let text = document.getElementById("text");
+function getvalue(){var text =Document.getElementById("text").value;
+
+}
+console.log(document.getElementById("text").value);
